0b5cf2066e274235ba23474ac2178da353617f07
Mbriito <mariabrito070@mail.com>
2021-02-01 21:40:44 -0300

    first commit

---
+++ Dicionarios/DicionariosExemplo.py
@@ -0,0 +1,11 @@
+usuarios = {}
+print(usuarios)
+
+usuarios = {
+    "MULHER-Maravilha" : ["Diana da Marvel", "12/12/2020", "Arquivos_de _Armas"],
+    "Super_Homem" : ["Clark Keent", "03/12/2020", "Arquivos_Poderes"],
+}
+print(usuarios)
+#inserir mais informaçoes dinamicamente
+usuarios["Hulk"] = ["David Benner", "24/12/2020", "Suprimentos_quimicos"]
+print(usuarios)
